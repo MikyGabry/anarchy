2c82f09ef2047b770fcad8423e111bc752bcc9b1
Miky.Gabry <isgromichael@gmail.com>
2023-03-30 13:32:08 -0400

    Updated with dr doom

--- controllers/characters.js
+++ controllers/characters.js
@@ -5,4 +5,9 @@ router.get('/professorchaos', (req, res) => {
     res.send('<h1> Nobody knows the secret identity of professor chaos! His evil knows no limits</h1>')
 })
 
+router.get('/doctordoom', (req, res) => {
+    res.send('<h1> Doctor doom is even more evil than butters. And has less issues with his parents </h1>')
+})
+
+
 module.express = router;
